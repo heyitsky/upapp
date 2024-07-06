268a0fcdf0cc956235afa00f364762e4ab46d3d5
Ky <p.kystaal@gmail.com>
2024-07-07 01:01:13 +1000

    Add transaction account type logic

--- upapp/account.py
+++ upapp/account.py
@@ -2,10 +2,15 @@ class Account:
     def __init__(self, type, id, name, ownership, balance):
         self.type = type
         self.id = id
-        self.name = name
         self.balance = balance
         self.ownership = ownership
-        self.emoji = name.split(' ')[0]
+        
+        if self.type == "TRANSACTIONAL":
+            self.emoji = "$$"
+            self.name = name
+        else:
+            self.emoji = name.split()[0]
+            self.name = " ".join(name.split()[1:])
     
     def __repr__(self):
         return f"<Account(id={self.id}, type={self.type}, name={self.name}, emoji={self.emoji}, ownership={self.ownership}, balance={self.balance}>"
